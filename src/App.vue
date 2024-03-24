873072bfbbc6b41cfc83cdc33b7122e17fd5661f
Danial J <dan90@live.com.my>
2024-03-24 07:02:08 +0000

    update user events

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,38 @@
 		{'id':2, label:'2 board games'},
 		{'id':3, label:'20 cups'}
 	])
+	const newItem = ref()
+	const newItemPriority = ref('low')
+	const newItemHighPriority = ref(false)
+	const iceCreamFlavors = ref([])
 </script>
 
 <template>
+	
 	<h1>{{ header }}</h1>
+	<form 
+		class="add-item-form"
+		@submit.prevent="items.push({id:items.length+1, label:newItem})"
+	>
+		
+		<input v-model.trim ="newItem" type="text" placeholder="Add an item">
+		
+	
+		<label>
+			<input type="checkbox" v-model="newItemHighPriority">High Priority
+		</label>
+
+		<button 
+			class="btn btn-primary">Save Item
+		</button>
+	</form>
+	
+	
+	
 	<ul>
 		<li v-for="({id,label}) in items" :key="id">{{label}} </li>
 	</ul>
+	
 </template>
 
 
